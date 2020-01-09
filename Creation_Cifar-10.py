2cf5e8d38f711a5ab503a5233e06fe9b91b138b2
Pistonomaxime <45484302+Pistonomaxime@users.noreply.github.com>
2020-01-09 16:40:02 +0100

    Quality of life and DC_AC_pur creation added

--- Creation_Cifar-10.py
+++ Creation_Cifar-10.py
@@ -2,28 +2,30 @@ from keras.datasets import cifar10
 from PIL import Image
 import os, sys
 
-if (len(sys.argv) != 2):
-	print('You need to choose a JPEG quality factor. Try 100 or 90 for example.')
-else:
-	qualite = int(sys.argv[1])
-	(X_train, y_train), (X_test, y_test) = cifar10.load_data()
-	current_path = os.getcwd()
-	dir_train_path = 'Cifar-10_{}'.format(qualite)
-	dir_test_path = 'Cifar-10_{}_test'.format(qualite)
-	os.mkdir(dir_train_path)
-	os.mkdir(dir_test_path)
+qualite = -1
+while (qualite > 100 or qualite < 0):
+	qualite = int(input("You need to choose a JPEG quality factor. Try 100 or 90 for example. \nQuality: "))
 
-	os.chdir(dir_train_path)
-	for i in range(len(X_train)):
-	   img = Image.fromarray(X_train[i])
-	   img = img.convert("L")
-	   nom = str(i) + ".jpg"
-	   img.save(nom, quality = qualite)
+(X_train, y_train), (X_test, y_test) = cifar10.load_data()
+current_path = os.getcwd()
+dir_train_path = 'Cifar-10_{}'.format(qualite)
+dir_test_path = 'Cifar-10_{}_test'.format(qualite)
+os.mkdir(dir_train_path)
+os.mkdir(dir_test_path)
 
-	os.chdir(current_path)
-	os.chdir(dir_test_path)
-	for i in range(len(X_test)):
-	    img = Image.fromarray(X_test[i])
-	    img = img.convert("L")
-	    nom = str(i) + ".jpg"
-	    img.save(nom, quality = qualite)
+os.chdir(dir_train_path)
+for i in range(len(X_train)):
+   img = Image.fromarray(X_train[i])
+   img = img.convert("L")
+   nom = str(i) + ".jpg"
+   img.save(nom, quality = qualite)
+
+os.chdir(current_path)
+os.chdir(dir_test_path)
+for i in range(len(X_test)):
+    img = Image.fromarray(X_test[i])
+    img = img.convert("L")
+    nom = str(i) + ".jpg"
+    img.save(nom, quality = qualite)
+
+print('Created')
