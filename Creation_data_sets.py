5cd0b803b9355f81e2ac3fa6781da0957d688498
Pistonomaxime <45484302+Pistonomaxime@users.noreply.github.com>
2020-01-09 17:29:30 +0100

    QoF plus _2 error change

--- Creation_data_sets.py
+++ Creation_data_sets.py
@@ -2,14 +2,14 @@ from PIL import Image
 import os, sys
 
 def convert_Mnist(dir_path, X):
-	os.chdir(dir_path)
+	os.chdir(dir_path + '/images')
 	for i in range(len(X)):
 	   img = Image.fromarray(X[i])
 	   nom = str(i) + ".jpg"
 	   img.save(nom, quality = qualite)
 
 def convert_Cifar(dir_path, X):
-	os.chdir(dir_path)
+	os.chdir(dir_path + '/images')
 	for i in range(len(X)):
 		img = Image.fromarray(X[i])
 		img = img.convert("L")
@@ -21,6 +21,17 @@ def convert(dir_path, X, dataset):
 		convert_Mnist(dir_path, X)
 	else:
 		convert_Cifar(dir_path, X)
+
+def create_directories(current_path, dir_train_path,dir_test_path):
+	os.mkdir(dir_train_path)
+	os.chdir(dir_train_path)
+	os.mkdir('images')
+	os.chdir(current_path)
+	os.mkdir(dir_test_path)
+	os.chdir(dir_test_path)
+	os.mkdir('images')
+	os.chdir(current_path)
+
 #main
 qualite = -1
 while (qualite > 100 or qualite < 0):
@@ -32,17 +43,16 @@ while (dataset != 0 and dataset != 1):
 if (dataset == 0):
 	from keras.datasets import mnist
 	(X_train, y_train), (X_test, y_test) = mnist.load_data()
-	dir_train_path = 'Mnist_{}_2'.format(qualite)
-	dir_test_path = 'Mnist_{}_test_2'.format(qualite)
+	dir_train_path = 'Mnist_{}'.format(qualite)
+	dir_test_path = 'Mnist_{}_test'.format(qualite)
 else:
 	from keras.datasets import cifar10
 	(X_train, y_train), (X_test, y_test) = cifar10.load_data()
-	dir_train_path = 'Cifar-10_{}_2'.format(qualite)
-	dir_test_path = 'Cifar-10_{}_test_2'.format(qualite)
+	dir_train_path = 'Cifar-10_{}'.format(qualite)
+	dir_test_path = 'Cifar-10_{}_test'.format(qualite)
 
 current_path = os.getcwd()
-os.mkdir(dir_train_path)
-os.mkdir(dir_test_path)
+create_directories(current_path, dir_train_path,dir_test_path)
 convert(dir_train_path, X_train, dataset)
 os.chdir(current_path)
 convert(dir_test_path, X_test, dataset)
