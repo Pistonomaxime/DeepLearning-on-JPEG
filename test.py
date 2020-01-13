d6d9a15a3e4147c3eed05cc086b37858529c69ce
Pistonomaxime <45484302+Pistonomaxime@users.noreply.github.com>
2020-01-13 15:57:08 +0100

    without errors

--- test.py
+++ test.py
@@ -5,7 +5,6 @@ def sha_images(dir_path):
     m = hashlib.sha256()
     os.chdir(dir_path + '/images')
     Tab_Document = glob.glob('*.jpg')
-    val = ''
     for i in range(len(Tab_Document)):
         Nom_de_photo = str(i) + '.jpg'
         with open(Nom_de_photo, 'rb') as f:
@@ -13,14 +12,14 @@ def sha_images(dir_path):
             m.update(im)
     return(m.hexdigest())
 
-def display_result(sha_images, sha_expected, name):
-    if (sha_images==sha_expected):
+def display_result(sha_result, sha_expected, name):
+    if (sha_result == sha_expected):
         print("Creation of", name, "is Ok !")
     else:
         print("Error during Images creation of ", name, "!!!")
 
-def partial_test(qualite, dataset, dir_train_path, dir_test_path):
-    sha_train_images = sha_images(dir_train_path) #pour moi rajouter + '_2' pour avoir les datasets avec 20 images
+def image_partial_test(qualite, dataset, dir_train_path, dir_test_path, current_path):
+    sha_train_images = sha_images(dir_train_path + '_2') #pour moi rajouter + '_2' pour avoir les datasets avec 20 images
     if (dataset == 0):
         if(qualite == 100):
             display_result(sha_train_images, '7c32810aac34b57cb9f45b5e9e36eff8b440efacec6064f7100332660bf32d2b', "train")
@@ -43,31 +42,32 @@ def partial_test(qualite, dataset, dir_train_path, dir_test_path):
             display_result(sha_train_images, 'e57a7d43e7ac3c36f3011764cd3a156126fd7db1f56c40540564b52f9a2689d9', "train")
         else:
             display_result(sha_train_images, '7077e9cf427573c27536eed378002ab9fdeba253fc680c32f7f5491eae156315', "train")
-    sha_test_images = sha_images(dir_test_path)
+    sha_test_images = sha_images(dir_test_path +  '_2')
     if (dataset == 0):
         if(qualite == 100):
-            display_result(sha_test_images, '0aa41c6eeb66c05a24ee8adbe64ea3bb3e1dc517d6d16fa04a3eaf613198b65d', "test")
+            display_result(sha_test_images, '09af6548c8dbbd398056b26f58a0d90c757cd4fe0f87b0c7f1cc67b690b1d737', "test")
         elif(qualite == 90):
-            display_result(sha_test_images, '6714cca013a66cbbf8e07442edb0b3ea3f14db6c6c74e5941f1020886ac755cc', "test")
+            display_result(sha_test_images, 'a732098654af441f9dacf2dbb5b8c6cb5afbd1c585f126f184dbeaa0055c1f61', "test")
         elif(qualite == 80):
-            display_result(sha_test_images, '6e1fb524f37ee314f9d39757ed8c129267e2dbefab37ff18769a895f8a3f2eff', "test")
+            display_result(sha_test_images, '61db475b118464655ace5ddb3112820f3549eea8907dcfc968ed3d446a3aed26', "test")
         elif(qualite == 70):
-            display_result(sha_test_images, '22faab6734b32799c1aceb991d2d03e360daea469395f83e00ae27e11959eac8', "test")
+            display_result(sha_test_images, 'd5ca91d3b6d50fd361a7dae9c011404f908e140cae1579211becf4e28cb6699b', "test")
         else:
-            display_result(sha_test_images, 'f15f792f7e1b277a51a30def6706f626fcc2ebb84b8f66a1365d747c0e83bcb0', "test")
+            display_result(sha_test_images, '5238a02e080fc843dac679fccf8cf94b1807da7fe8b81f731ecf5f32f6230677', "test")
     else:
         if(qualite == 100):
-            display_result(sha_test_images, '96ee21e69cb01262bb10aaee2a862dabcfc09f355086190544c85de4babdf309', "test")
+            display_result(sha_test_images, 'fd8253bdf4cf3a37064a681acd88274e2265c3b4ea0b7147f8883948bc478939', "test")
         elif(qualite == 90):
-            display_result(sha_test_images, '03c007ba61e8788bd47192312d3f5ef36b8de3dd560d999dcc1ab9ed28031f60', "test")
+            display_result(sha_test_images, '043805635c7008b5c6dae5e1121a5dc982e69c37a3a9d9ffd0def00090ba1bc9', "test")
         elif(qualite == 80):
-            display_result(sha_test_images, '2ef45238c362d18a398d704d2496b03dece092b01580d0bd457fcb1e554b87a4', "test")
+            display_result(sha_test_images, 'a1d113344d3ac9c6030477b346ba0dc33aa168a38c8043bbf4d4f2ce6a3ccb00', "test")
         elif(qualite == 70):
-            display_result(sha_test_images, 'e733a5b3f08c3303f6b21a4fa5bf90738263b726ac017f7e391197246fdd3b39', "test")
+            display_result(sha_test_images, '0dddccb05bb3398b4a61118fc8b663faae1d17e6c9e2c91ba4b489e755ac153e', "test")
         else:
-            display_result(sha_test_images, 'a051f725909d6f07ee78ac676ff3d78f4d330737d31b252ab4eabb8b97a14009', "test")
-    
-def full_test(qualite, dataset, dir_train_path, dir_test_path):
+            display_result(sha_test_images, 'ca0f6a430c583b14800dfaf26dc2766fec11de194fd5b647d5473980b53ac8e3', "test")
+    os.chdir(current_path)
+
+def image_full_test(qualite, dataset, dir_train_path, dir_test_path, current_path):
     sha_train_images = sha_images(dir_train_path)
     if (dataset == 0):
         if(qualite == 100):
@@ -114,6 +114,23 @@ def full_test(qualite, dataset, dir_train_path, dir_test_path):
             display_result(sha_test_images, 'e733a5b3f08c3303f6b21a4fa5bf90738263b726ac017f7e391197246fdd3b39', "test")
         else:
             display_result(sha_test_images, 'a051f725909d6f07ee78ac676ff3d78f4d330737d31b252ab4eabb8b97a14009', "test")
+    os.chdir(current_path)
+
+def data_DC_AC_partial_test(dir_train_path, dir_test_path):
+    os.chdir(dir_train_path)
+    m = hashlib.sha256()
+    with open("data_DC_AC_pur.txt", 'rb') as f:
+        # im = f.read()
+        m.update(f)
+    print(m.hexdigest())
+    return(m.hexdigest())
+
+def partial_test(qualite, dataset, dir_train_path, dir_test_path, current_path):
+    image_partial_test(qualite, dataset, dir_train_path, dir_test_path, current_path)
+    # data_DC_AC_partial_test(dir_train_path, dir_test_path)
+
+def full_test(qualite, dataset, dir_train_path, dir_test_path, current_path):
+    image_full_test(qualite, dataset, dir_train_path, dir_test_path, current_path)
 
 def main_test(qualite, dataset, partial = True):
     current_path = os.getcwd()
@@ -124,9 +141,9 @@ def main_test(qualite, dataset, partial = True):
         dir_train_path = current_path + '/Cifar-10_{}'.format(qualite)
         dir_test_path = current_path + '/Cifar-10_{}_test'.format(qualite)
     if (partial):
-        partial_test(qualite, dataset, dir_train_path, dir_test_path)
+        partial_test(qualite, dataset, dir_train_path, dir_test_path, current_path)
     else:
-        full_test(qualite, dataset, dir_train_path, dir_test_path)
+        full_test(qualite, dataset, dir_train_path, dir_test_path, current_path)
     os.chdir(current_path)
 
 # from test import main_test
